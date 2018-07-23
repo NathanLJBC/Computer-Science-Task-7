4a31a7cebf0fee675db3770898c42105039ebbaf
Cryze <NathanLJBC@users.noreply.github.com>
2018-07-23 11:05:16 +0800

    Computer Science
    mmmmm

---
+++ js.js
@@ -0,0 +1,12 @@
+function Amarok1() {
+    document.getElementById("myImg").src = "hackanm.gif";
+}
+function Amarok2() {
+    document.getElementById("myImg").src = "hackanm.gif";
+}
+function Amarok3() {
+    document.getElementById("myImg").src = "hackanm.gif";
+}
+function Amarok4() {
+    document.getElementById("myImg").src = "hackanm.gif";
+}
